c871c35d2dd0726513ec471559068ad62a34e9be
steve <stevesiroki@gmail.com>
2012-11-28 21:59:23 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BOOST_ROOT="/usr/local"
+
+mkdir -p bin
+cd bin
+cmake .. \
+  -DCMAKE_CXX_COMPILER:STRING="/usr/bin/g++" \
+  -DCMAKE_CXX_FLAGS:STRING="-std=c++0x -I$BOOST_ROOT/include -L$BOOST_ROOT/lib"
+
+export LD_LIBRARY_PATH=$BOOST_ROOT/lib
+make $*
+
